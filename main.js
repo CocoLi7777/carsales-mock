2f20bdafde4d42e5473bf7649f5186d5ea167b54
Coco <coco.li7777@gmail.com>
2020-07-26 19:44:40 +1000

    build showroom area

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+/* showcase area */
+
 $(document).ready(function () {
   $('.default-option').click(function () {
     $(this).parent().toggleClass('active');
@@ -10,4 +12,26 @@ $(document).ready(function () {
     $(this).parent().siblings().children().html(currentele);
     $('.search-field').removeClass('active');
   });
+});
+
+/* showroom area */
+const h5Items = document.querySelectorAll('.h5');
+const showroomContents = document.querySelectorAll('.showroom-content');
+
+function selectItem(e) {
+  removeShow();
+  removeActive();
+  const showroomContentsItem = document.querySelector(`#${this.id}-content`);
+  showroomContentsItem.classList.add('show');
+  this.classList.add('active');
+}
+function removeShow() {
+  showroomContents.forEach((item) => item.classList.remove('show'));
+}
+function removeActive() {
+  h5Items.forEach((item) => item.classList.remove('active'));
+}
+
+h5Items.forEach((item) => {
+  item.addEventListener('click', selectItem);
 });
